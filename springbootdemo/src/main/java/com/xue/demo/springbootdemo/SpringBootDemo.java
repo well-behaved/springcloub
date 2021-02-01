f7135b8f734f55c93b02a70d91425020b45a7f46
“xuexiong” <xuexiong@souche.com>
2021-02-01 18:02:48 +0800

    增加springboot例子 供研究使用

---
+++ springbootdemo/src/main/java/com/xue/demo/springbootdemo/SpringBootDemo.java
@@ -0,0 +1,16 @@
+package com.xue.demo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: xuexiong@souche.com
+ * @date: 2021/2/1 18:01
+ * @description: 启动类
+ */
+@SpringBootApplication
+public class SpringBootDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo.class, args);
+    }
+}
